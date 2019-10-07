cab60e8e1a4c60a5f09ffb1d92e8be8f69c493be
Richie47 <RichieMck47@gmail.com>
2019-10-06 19:41:36 -0700

    implemented javadoc comments

---
+++ src/cse360assign2/AddingMachine.java
@@ -0,0 +1,58 @@
+/**
+ * @author Richard McKinnon
+ * CSE360 M 940am
+ * Assignment #2
+ * Description: AddingMachine performs basic arithmitic such as adding a subtraction, and a few other helper methods such as
+ * retrieving the total, getting a toString() version of the number, and clearing the number. 
+ *
+ */
+
+package cse360assign2;
+/**
+ * 
+ * @author Richie
+ * This class acts a machine that can add, subtract, retrieve the total, get a toString representation and clear the total
+ */
+public class AddingMachine {
+
+	private int total;
+	/*
+	 * Constructor for class AddingMachine
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	/*
+	 * gets the total amount
+	 * @return the total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	/*
+	 * adds an arbitrary value to the total
+	 * @param value an int to be added to total
+	 */
+	public void add (int value) {
+		
+	}
+	/*subtracts an arbitrary value to the total
+	 * @param value an int to be subtracted from the total
+	 */
+	public void subtract (int value) {
+		
+	}
+	/*
+	 * get a string representation of the total
+	 * @return empty string
+	 */
+	public String toString () {
+		return "";
+	}
+	/*
+	 * reset total back to 0
+	 */
+	public void clear() {
+	
+	}
+}
